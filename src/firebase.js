1f44c789bf4881cfdd79ca6abf3fbd72e5924993
nzimmerman3 <nicholaszimmerman1998@gmail.com>
2020-09-05 12:43:17 -0700

    Changed env

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,6 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
 
-// const app = firebase.initializeApp({
-//   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-// });
-
-// export default app;
-
 var firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
